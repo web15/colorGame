a7230bc9ee27a84efb52cd2c66e7eb140fc8541e
Charles Marks <clmarks15@gmail.com>
2019-01-08 16:26:29 -0500

    readyPart4

--- script.js
+++ script.js
@@ -10,11 +10,40 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    // add click listeners to squares
+    squares[i].addEventListener('click', function() {
+        // grab color of picked square
+        var clickedColor = this.style.backgroundColor;
+        //compare color to picked square
+        if(clickedColor === pickedColor)    {
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
+        }
+    });
+}
+
+function changeColors(color)    {
+    // Loop through all squares
+    for(var i = 0; i < squares.length; i++)  {
+        // Change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor()    {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
