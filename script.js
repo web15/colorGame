275cdfad6d1f1418384fa6aad0e6def107c6efac
Charles Marks <clmarks15@gmail.com>
2019-01-08 16:50:31 -0500

    readyPart5

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 //alert("working");
 
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255 , 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 =  document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++) {
+for (var i = 0; i < squares.length; i++) {
     //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
 
     // add click listeners to squares
-    squares[i].addEventListener('click', function() {
+    squares[i].addEventListener('click', function () {
         // grab color of picked square
         var clickedColor = this.style.backgroundColor;
         //compare color to picked square
-        if(clickedColor === pickedColor)    {
+        if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
@@ -35,15 +29,37 @@ for(var i = 0; i < squares.length; i++) {
     });
 }
 
-function changeColors(color)    {
+function changeColors(color) {
     // Loop through all squares
-    for(var i = 0; i < squares.length; i++)  {
+    for (var i = 0; i < squares.length; i++) {
         // Change each color to match given color
         squares[i].style.backgroundColor = color;
     }
 }
 
-function pickColor()    {
+function pickColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    // make an array
+    var arr = []
+    // add num random colors to array
+    for (var i = 0; i < num; ++i) {
+     // get random color and push into arr
+        arr.push(randomColor())
+    }
+    // return that array
+    return arr;
+}
+
+function randomColor() {
+    // pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    // pick a "green" from 0 - 255
+    var g = Math.floor(Math.random() * 256);
+    // pick a "blue" from 0 - 255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
