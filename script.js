187911c389379a610d86b89e957d63f1ddaa0197
Charles Marks <clmarks15@gmail.com>
2019-01-09 10:15:52 -0500

    b4refactor

--- script.js
+++ script.js
@@ -48,6 +48,9 @@ resetButton.addEventListener('click', function() {
     pickedColor = pickColor();
     //change colorDisplay to match picked color
     colorDisplay.textContent = pickedColor;
+    this.textContent = "New Colors";
+    // remove 'Correct!' when new game
+    messageDisplay.textContent = "";
     // change colors of squares
     for(var i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
